c064bc329504546d837b3325058ac023653d73ba
Denis Danilchuk <ddanilchuk13@gmail.com>
2020-06-13 19:52:18 +0300

    Clean up view bindings when fragment view destroyed

--- app/src/main/java/com/valhalla/lolchampviewer/ui/core/Bindings.kt
+++ app/src/main/java/com/valhalla/lolchampviewer/ui/core/Bindings.kt
@@ -2,10 +2,48 @@ package com.valhalla.lolchampviewer.ui.core
 
 import android.view.View
 import androidx.annotation.IdRes
+import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
-fun <T : View> Fragment.bindView(@IdRes id: Int): Lazy<T?> = lazy { view?.findViewById<T>(id) }
+fun <T : View> bindView(@IdRes id: Int): FragmentViewBindingProperty<T> =
+    FragmentViewBindingProperty(id)
 
 fun Fragment.onClick(@IdRes id: Int, onClick: (View) -> Unit) {
     view?.findViewById<View>(id)?.setOnClickListener(onClick)
+}
+
+class FragmentViewBindingProperty<T : View>(
+    @IdRes private val viewId: Int
+) : ReadOnlyProperty<Fragment, T?> {
+
+    private var view: T? = null
+    private var isAssigned: Boolean = false
+    private val lifecycleObserver = BindingLifecycleObserver()
+
+    @MainThread
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T? {
+
+        if (isAssigned) {
+            return view
+        }
+
+        thisRef.viewLifecycleOwner.lifecycle.addObserver(lifecycleObserver)
+
+        view = thisRef.requireView().findViewById(viewId)
+        isAssigned = true
+        return view
+    }
+
+    private inner class BindingLifecycleObserver : DefaultLifecycleObserver {
+        override fun onDestroy(owner: LifecycleOwner) {
+            owner.lifecycle.removeObserver(this)
+            view = null
+            isAssigned = false
+        }
+    }
+
 }
